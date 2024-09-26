d1359fa7693c7e82ed658edf08eda57fe7dd316a
Navyrine <wahidadinugrohoo@gmail.com>
2024-09-26 19:26:17 +0800

    membuat dummy data untuk category

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,10 @@
+import 'package:cultivated_plants_app/model/category.dart';
+import 'package:flutter/material.dart';
+
+const availableCategories = [
+  Category(id: "c1", title: "Pangan", color: Colors.purple),
+  Category(id: "c2", title: "Holtikutura", color: Colors.purple),
+  Category(id: "c3", title: "Hias", color: Colors.red),
+  Category(id: "c4", title: "Obat", color: Colors.orange),
+  Category(id: "c5", title: "Industri", color: Colors.amber),
+];
